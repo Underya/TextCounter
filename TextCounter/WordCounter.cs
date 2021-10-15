f3f57b07c4d43d0951d6daeee389ebd5df7ede9f
Matveev Nikita Ivanovich <nik.nikita.matveev.1997@gmail.com>
2021-10-16 00:09:11 +0500

    Добавлен класс для простого (без обработки) подсчёта слов

---
+++ TextCounter/WordCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextCounter
+{
+    public class WordCounter
+    {
+        public Dictionary<string, int> Count(IEnumerable<string> wordList)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (string word in wordList)
+                UpdateWordWalue(dict, word);
+            return dict;
+        }
+        void UpdateWordWalue(Dictionary<string, int> dict, string word)
+        {
+            int value;
+            if (dict.TryGetValue(word, out value))
+                dict[word] = ++value;
+            else
+                dict[word] = 1;
+        }
+    }
+}
---
+++ TextCounter_test/WordCounter_test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using TextCounter;
+
+using Moq;
+using NUnit.Framework;
+
+namespace TextCounter_test
+{
+
+    [TestFixture]
+    public class WordCounter_test
+    {
+        [Test]
+        public void
+        Counter_CounterOneWord_ReturnSameWord()
+        {
+            WordCounter counter = new WordCounter();
+            List<string> wordList = new List<string> { "word1", "word2", "word1"};
+
+            Dictionary<string, int> keyCount = counter.Count(wordList);
+
+            Assert.Contains("word1", keyCount.Keys);
+            Assert.Contains("word2", keyCount.Keys);
+        }
+        
+        [Test]
+        public void
+        Counter_CounterWord_ReturnRightCountWords()
+        {
+            WordCounter counter = new WordCounter();
+            List<string> wordList = new List<string> { "word3", "word2", "word1", "word3", "word2", "word3" };
+
+            Dictionary<string, int> keyCount = counter.Count(wordList);
+
+            Assert.AreEqual(3, keyCount["word3"]);
+            Assert.AreEqual(2, keyCount["word2"]);
+            Assert.AreEqual(1, keyCount["word1"]);
+        }
+
+    }
+}
